4ef30bfa88e16e95e7fa7b0b9e04cc21e4dac9fc
Levelvini <viniciusfc29@gmail.com>
2023-12-11 09:00:41 -0300

    files scanner

---
+++ src/program/App.java
@@ -0,0 +1,28 @@
+package program;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+
+        File file = new File("\"C:\\Users\\Level_vini\\Downloads\\carta de solicitacao LCGH EDSON NEVES.docx\"");
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()){
+                System.out.println(sc.nextLine());
+            }
+        } catch (IOException e) {
+            System.out.println("error: " + e.getMessage());
+        }
+        finally {
+            if (sc != null){
+                sc.close();
+            }
+        }
+    }
+}
+
